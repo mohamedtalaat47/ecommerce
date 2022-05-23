bad1d5ec33cbc236efa5c3ecd1d6bc6866611ee2
Mohamed Talaat <mohamed_talaat_47@yahoo.com>
2022-05-23 04:11:43 +0200

    added attributes, attribute values

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
         $this->call(AdminsTableSeeder::class);
         $this->call(SettingsTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
+        $this->call(AttributesTableSeeder::class);
 
 
     }
